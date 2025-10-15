ee6c914de151c9bcc8adfcf6c84a5f6c2cc0972a
cesaringuz15 <cesar.guzman@enjoy.cl>
2025-10-15 16:56:56 -0300

    commit rama develop/desaVterinario

--- src/main/java/com/example/veterinaria/controller/VeterinariaController.java
+++ src/main/java/com/example/veterinaria/controller/VeterinariaController.java
@@ -67,7 +67,7 @@ public class VeterinariaController {
 		clienteService.eliminar(id);
 	}
 	
-	//Mascota test
+	//Mascota cambio para rfama dev
 	@GetMapping(value = "/mascota/listar")
 	public ResponseEntity <List<Mascotas>> getMascotas() {
 		 return ResponseEntity.ok(mascotaService.getMacotas());
